9fe3afc5b4051ce39f9f6408bb6a4cf25d64d6d5
oz527 <118091285+oz527@users.noreply.github.com>
2023-01-06 14:22:22 +0900

    study

---
+++ app.js
@@ -0,0 +1,12 @@
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleH1Click() {
+  const clickedClass = "clicked";
+  if (h1.className === clickedClass) {
+    h1.className = "";
+  } else {
+    h1.className = clickedClass;
+  }
+}
+
+h1.addEventListener("click", handleH1Click);
